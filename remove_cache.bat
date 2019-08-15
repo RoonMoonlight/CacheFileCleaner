38ecab0a9a9abb9ef3c8bbd2e639a92b4912b8db
Ah_Roon <ahroon2979@gmail.com>
2019-08-15 11:48:39 +0900

    Add files via upload

--- remove_cache.bat
+++ remove_cache.bat
@@ -4,8 +4,21 @@ echo ------------------------------------------------------
 echo                  Cache file Remover
 echo                      By Ah_Roon
 echo ------------------------------------------------------
+IF NOT EXIST "..\..\hl2.exe" (goto :ERR_NO_CUSTOM_FOLDER)
+goto :CLEAN
+:ERR_NO_CUSTOM_FOLDER
+echo Please put this batch file on custom folder to run this script.
+echo ------------------------------------------------------
+echo Error Code: E01 (ERR_NO_CUSTOM_FOLDER)
+goto EXIT
+
+:CLEAN
+echo Cleaning all cache files.
 del /F /S *.cache
 echo ------------------------------------------------------
 echo Removed all Cache Folder!
+goto EXIT
+
+:EXIT
 pause
 exit
