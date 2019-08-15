43b9fec784af95fb01b95f184121b7202d833a87
Ah_Roon <ahroon2979@gmail.com>
2019-08-15 11:17:03 +0900

    Add files via upload

---
+++ remove_cache.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+echo ------------------------------------------------------
+echo                  Cache file Remover
+echo                      By Ah_Roon
+echo ------------------------------------------------------
+del /F /S *.cache
+echo ------------------------------------------------------
+echo Removed all Cache Folder!
+pause
+exit
